38c77eb1d8f2d478b798a53c797b7d88e97b9153
Arthur <artopenheimervilela@gmail.com>
2023-08-29 21:04:42 -0300

    feat: adicionar exercicios da aula 5 do laboratório

---
+++ Laboratorio/Aula5/Estudante.java
@@ -0,0 +1,15 @@
+package Aula5;
+
+public class Estudante {
+    
+    String nome;
+    int idade;
+    String anoLetivo;
+
+    void mostrarInfos(){
+        System.out.println("#####Informações do Estudante#####");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano Letivo: " + anoLetivo);
+    }
+}
